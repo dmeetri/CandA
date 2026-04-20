9d32a7bad5534e82d7f89ad6741f5bf744a0d41a
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-04-20 20:55:14 +0900

    add - rename upload files

--- CN/main/metrics.py
+++ CN/main/metrics.py
@@ -4,11 +4,13 @@ from .services import send_email_message
 from django.conf import settings
 
 def get_disk_usega(path='/'):
+    '''
     result = send_email_message(
         to_email=settings.EMAIL_FOR_SEND,
         subject='C&A WARNING',
         message="efnjn"
     )
+    '''
 
     total, used, free = shutil.disk_usage(path)
 
---
+++ CN/main/signals.py
@@ -0,0 +1,15 @@
+import os
+import uuid
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import FileModel
+
+@receiver(pre_save, sender=FileModel)
+def rename_file_before_save(sender, instance, **kwargs):
+    if instance.file:
+        old_file = instance.file
+
+        ext = os.path.splitext(old_file.name)[1]
+        new_filename = f"{uuid.uuid4().hex}{ext}"
+
+        instance.file.name = os.path.join('files/', new_filename)
