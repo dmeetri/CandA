a315d67885b6ad1c82f5e0b676f877fd0ade9177
dmeetri <dmitkorenkey@gmail.com>
2026-04-20 13:04:25 +0900

    add model for share files

--- CN/main/models.py
+++ CN/main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, User
 
 class CustomUser(AbstractUser):
     first_name = None
@@ -50,3 +50,20 @@ class FileModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class FileForUserModel(models.Model):
+    file = models.ForeignKey(
+        FileModel,
+        on_delete=models.CASCADE,
+    )
+    sender = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='sent_files'
+    )
+    recipient = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='received_files'
+    )
