a81a220404b4d978494d44e7d1ef449a879c02bc
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-04-21 19:05:57 +0900

    add database on fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,27 @@
 import uvicorn
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from api.deps import get_db
+from models.user import User
+from schemas.user import UserCreate, UserResponse
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/users", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = User(email=user.email, name=user.name)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/users")
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
 
 if __name__ == '__main__':
     uvicorn.run(
