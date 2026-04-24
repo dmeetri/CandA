ca355c7e4d1b1103e78b7909423c0b73b1708688
dmeetri <dmitkorenkey@gmail.com>
2026-04-24 13:08:34 +0900

    fix: chache page

--- CN/main/signals.py
+++ CN/main/signals.py
@@ -1,10 +1,15 @@
 import os
 import uuid
 
-from django.db.models.signals import pre_save
+from django.core.cache import cache
+from django.db.models.signals import pre_save, post_delete, post_save
 from django.dispatch import receiver
 
 from .models import FileModel
+
+@receiver([post_save, post_delete], sender=FileModel)
+def cache_clean(sender, instance, **kwargs):
+    cache.clear()
 
 @receiver(pre_save, sender=FileModel)
 def rename_file_before_save(sender, instance, **kwargs):
