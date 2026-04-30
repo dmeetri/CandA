755017042f268457b98abd58662ff7fdad20b5fd
dmeetri <145245244+dmeetri@users.noreply.github.com>
2026-04-30 21:49:21 +0900

    add STATICFILES_STORAGE

--- CN/CN/settings.py
+++ CN/CN/settings.py
@@ -85,6 +85,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static/'
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # === NGINX ===
 
